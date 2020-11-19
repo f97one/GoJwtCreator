f81013776d40d14f78b28795cdda4c8465f24272
HAJIME Fukuna <f97one@gmail.com>
2020-11-19 21:06:39 +0900

    出力の最後に開業をつけない処理とヘルプ表示の処理を追加。

--- main.go
+++ main.go
@@ -10,8 +10,15 @@ func main() {
 	privateKeyFile := flag.String("private", "", "Private Key file to sign JWT (required)")
 	issuer := flag.String("issuer", "", "issuer string (required)")
 	expires := flag.Int("expires", 30, "Time to expiration (in minutes, 30 minutes if not specified)")
+	noLf := flag.Bool("nolf", false, "not append LF to end of line")
+	help := flag.Bool("help", false, "Display usage and exit")
 
 	flag.Parse()
+
+	if *help {
+		printUsage()
+		os.Exit(0)
+	}
 
 	if len(*privateKeyFile) == 0 {
 		fmt.Println("Private Key not specified.")
@@ -37,11 +44,15 @@ func main() {
 		os.Exit(2)
 	}
 
-	fmt.Println(ret)
+	if *noLf {
+		fmt.Print(ret)
+	} else {
+		fmt.Println(ret)
+	}
 	os.Exit(0)
 }
 
 func printUsage() {
-	fmt.Printf("usage : %s {-expires [expires]} -issuer [issuer] -private [private]\n", os.Args[0])
+	fmt.Printf("usage : %s {-expires [expires]} -issuer [issuer] {-nolf} -private [private]\n", os.Args[0])
 	flag.PrintDefaults()
 }
